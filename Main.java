1d0979d131aa79c5e6508635b45d7631781c9081
Yash Karnik <yashkarnik2000@gmail.com>
2022-04-26 12:48:27 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main
 {
 	public static void main(String[] args) {
 	    System.out.println("Hello");
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 20; i++) {
             System.out.print(i+" ");
         }
 	}
